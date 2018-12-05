d9582f2c8cdd7b42acc92e0811d9a58be92c8cac
Pierre Chabannon <pierrec@mbp-de-pierre.lan>
2018-12-05 16:32:56 +0100

    front pour add company et offer

--- src/App.js
+++ src/App.js
@@ -1,28 +1,61 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from "react";
+import "./App.css";
+import Home from "./components/Home";
+import Header from "./components/Header";
+import NewOffer from "./components/NewOffer";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Cookies from "js-cookie";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+class App extends React.Component {
+	state = {
+		company: {
+			token: Cookies.get("token") || "",
+			companyName: Cookies.get("companyName") || "",
+			_id: Cookies.get("_id") || ""
+		}
+	};
+	logIn = company => {
+		Cookies.set("token", company.token);
+		Cookies.set("companyName", company.companyName);
+		Cookies.set("_id", company._id);
+
+		this.setState({ company: company });
+	};
+
+	logOut = () => {
+		Cookies.remove("token");
+		Cookies.remove("companyName");
+		Cookies.remove("_id");
+
+		this.setState({
+			company: {
+				token: "",
+				companyName: "",
+				_id: ""
+			}
+		});
+	};
+	render() {
+		const { company } = this.state;
+		return (
+			<Router>
+				<Fragment>
+					<Header company={company} logOut={this.logOut} />
+					<Route
+						exact
+						path="/"
+						render={props => (
+							<Home {...props} company={company} logIn={this.logIn} />
+						)}
+					/>
+					<Route
+						path="/NewOffer"
+						render={props => <NewOffer {...props} company={company} />}
+					/>
+				</Fragment>
+			</Router>
+		);
+	}
 }
 
 export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,106 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+class Home extends Component {
+	state = {
+		email: "",
+		password: "",
+		companyName: ""
+	};
+	handleChange = event => {
+		const target = event.target;
+		const name = target.name;
+		// Utile si le formulaire contient des éléments "checkbox"
+		const value = target.type === "checkbox" ? target.checked : target.value;
+		this.setState({ [name]: value });
+	};
+	onSubmitLogin = event => {
+		axios
+			.post("http://localhost:3001/log_in_company", {
+				email: this.state.email,
+				password: this.state.password
+			})
+			.then(response => {
+				if (response.data && response.data.token) {
+					// console.log(response);
+					this.props.logIn({
+						token: response.data.token,
+						companyName: response.data.companyAccount.companyName,
+						_id: response.data._id
+					});
+					this.props.history.push("/NewOffer");
+				}
+			})
+			.catch(function(error) {
+				console.log(error);
+			});
+	};
+	onSubmitSignup = event => {
+		axios
+			.post("http://localhost:3001/sign_up_company", {
+				email: this.state.email,
+				password: this.state.password,
+				companyName: this.state.companyName
+			})
+			.then(response => {
+				if (response.data && response.data.token) {
+					console.log(response.data);
+					this.props.logIn({
+						token: response.data.token,
+						companyName: response.data.companyAccount.companyName,
+						_id: response.data._id
+					});
+					this.props.history.push("/NewOffer");
+				}
+			})
+			.catch(err => {
+				console.log(err);
+			});
+	};
+	render() {
+		return (
+			<div>
+				<h2>Se connecter</h2>
+				<div>
+					<input
+						name="email"
+						placeholder="email"
+						value={this.state.email}
+						onChange={this.handleChange}
+					/>
+					<input
+						name="password"
+						placeholder="password"
+						value={this.state.password}
+						onChange={this.handleChange}
+					/>
+					<button onClick={this.onSubmitLogin}>Se connecter</button>
+				</div>
+				<h2>Creer un compte</h2>
+				<div>
+					<input
+						name="email"
+						placeholder="email"
+						value={this.state.email}
+						onChange={this.handleChange}
+					/>
+					<input
+						name="password"
+						placeholder="password"
+						value={this.state.password}
+						onChange={this.handleChange}
+					/>
+					<input
+						name="companyName"
+						placeholder="companyName"
+						value={this.state.companyName}
+						onChange={this.handleChange}
+					/>
+					<button onClick={this.onSubmitSignup}>Valider</button>
+				</div>
+			</div>
+		);
+	}
+}
+
+export default Home;
